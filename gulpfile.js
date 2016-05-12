ed0a2e514f0f545aa7865d7420e43609244b11a2
glc12125 <Liangchuan Gu>
2016-05-12 22:48:25 +0100

    minify css and source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('styles', function(){
+	gulp.src(['src/styles/**/*.css'])
+		.pipe(sourcemaps.init())
+		.pipe(minifyCss())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(browserSync.stream())
+});
 
 gulp.task('default', function(){
 	browserSync.init({
 		server: './'
 	});
-	gulp.watch('src/**/*', browserSync.reload);
+	//gulp.watch('src/**/*', browserSync.reload);
+	gulp.watch('src/styles/**/*.css', ['styles']);
 	gulp.watch('*.html', browserSync.reload);
-})
+});
