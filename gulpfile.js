5a1b9a4ef078fde77dbd4a63d26f07d1ac382af9
glc12125 <Liangchuan Gu>
2016-05-15 18:23:17 +0100

    preprocess less files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var imageMin = require('gulp-imagemin');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
 
 var menu = require('./menu.json');
 
@@ -44,8 +45,9 @@ gulp.task('scripts', function(){
 });
 
 gulp.task('styles', function(){
-	gulp.src(['src/styles/**/*.css'])
+	gulp.src(['src/styles/main.less'])
 		.pipe(sourcemaps.init())
+		.pipe(less())
 		.pipe(minifyCss())
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/styles'))
@@ -57,7 +59,7 @@ gulp.task('default', ['styles', 'images', 'scripts', 'templates'], function(){
 		server: './'
 	});
 	//gulp.watch('src/**/*', browserSync.reload);
-	gulp.watch('src/styles/**/*.css', ['styles']);
+	gulp.watch('src/styles/**/*.less', ['styles']);
 	gulp.watch('src/scripts/**/*.js', ['scripts']);
 	gulp.watch('src/img/**/*', ['images']);
 	gulp.watch('src/templates/**/*.hbs', ['templates']);
