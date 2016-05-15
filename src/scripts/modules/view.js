cb8e692d80d4959e2f4e21e34974f081d35b4c00
glc12125 <Liangchuan Gu>
2016-05-15 23:07:13 +0100

    add menu click functionality

--- src/scripts/modules/view.js
+++ src/scripts/modules/view.js
@@ -3,6 +3,31 @@ var helper = require('./helper.js');
 var view = module.exports = {
   init: function() {
     view.showCountdown();
+    view.registerClick();
+  },
+
+  registerClick: function() {
+    var id;
+    var listItem = document.querySelectorAll('#menu li');
+
+    for(var i = 0; i < listItem.length; i++) {
+      listItem[i].addEventListener('click', function(){
+        id = this.getAttribute('data-id');
+        helper.setCurrent(+id);
+        helper.increment();
+        view.updateCount();
+      });
+    }
+  },
+
+  updateCount: function() {
+    var item = helper.getCurrent();
+    // find the li of the current menu item
+    var li = document.querySelector('[data-id="' + item.id + '"]');
+    var count = li.querySelector('.menu-count');
+    // update the count in the view
+    count.innerHTML = item.count;
+
   },
 
   showCountdown: function() {
